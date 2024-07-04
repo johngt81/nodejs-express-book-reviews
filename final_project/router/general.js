29a2a3ffb45ab4d426977b89fcd670e40db03c28
John Bryan Gaona Trinidad <john.gt81@gmail.com>
2024-07-03 19:12:02 -0500

    use async await and promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -23,30 +22,54 @@ public_users.post("/register", (req,res) => {
   }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
+
+const getBookList = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books);
+    }, 1000);
+  });
+};
+
+public_users.get('/', async function (req, res) {
+  const books = await getBookList();
   res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+const getBookByISBN = (isbn) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(books[isbn]);
+    }, 1000);
+  });
+};
 
-  const book = books[isbn];
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn;
+  const book = await getBookByISBN(isbn);
 
   if (!book)
   {
     return res.status(404).json({message: "Book not found"});
   }
   return res.status(200).json(book);
-// res.send(book);
-  // return res.status(200, book);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+
+const getBooksByAuthor = (author) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(Object.values(books).filter(book => book.author == author));
+    }, 1000);
+  });
+};
+
+public_users.get('/author/:author',async function (req, res) {
   const author = req.params.author;
-  const bookFounds = Object.values(books).filter(book => book.author == author);
+  const bookFounds = await getBooksByAuthor(author);
+  
   if (bookFounds.length > 0){
     return res.status(200).json(bookFounds);
   }
@@ -54,9 +77,18 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) { 
+
+const getBooksByTitle = (title) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(Object.values(books).filter(book => book.title == title));
+    }, 1000);
+  });
+};
+
+public_users.get('/title/:title', async function (req, res) { 
   const title = req.params.title;
-  const bookFounds = Object.values(books).filter(book => book.title == title);
+  const bookFounds = await getBooksByTitle(title);
   if (bookFounds.length > 0){
     return res.status(200).json(bookFounds);
   }
